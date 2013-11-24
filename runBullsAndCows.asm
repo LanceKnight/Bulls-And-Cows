be9fa5cce5d6925a8fad4417122a380211b32ca7
azouariUTD <asz130230@utdallas.edu>
2013-11-24 16:20:40 -0600

    Update runBullsAndCows.asm

--- runBullsAndCows.asm
+++ runBullsAndCows.asm
@@ -25,3 +25,9 @@ endOfProgram:
 userInput: .asciiz   "\nPlease enter a positive number between 0000 and FFFF (4-DIGIT): "
 userString: .space 16
 
+#checkForBullsAndCows
+winString: .asciiz "============================\n       Yeah! You win!\n============================\n\n    (__)\n    /oo\\\\________\n    \\  /          \\---\\\n     \\/        /   \\   \\\n        \\\\_|___\\\\_|/    *\n         ||    YY|\n         ||     ||\n"
+thereAre: .asciiz " There are "
+bullString: .asciiz " bulls.\n"
+cowString: .asciiz " cows.\n"
+
