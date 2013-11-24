5a6efe54c95f7f5754f4e686733b6965843eccbb
Ahmed Zouari <asz130230@utdallas.edu>
2013-11-24 14:25:25 -0600

    Add getUserInput.asm

--- getUserInput.asm
+++ getUserInput.asm
@@ -1,7 +1,15 @@
 	.text
 	
 getUserInput:
-	la $a0 , userInput #load address of userInput into $a0
-	li $v0 , 4 	  #Print String
+	la $a0 , userInput          #load address of userInput into $a0
+	li $v0 , 4 	           #Print String
 	syscall
+	
+	la $a0 , userString     #address if input buffer
+	li $a1 , 6  	       #number of char
+	li $v0 , 8           #Read the string
+	syscall
+	
+	la $s0 , userString #mover the content to $s0
 	jr $ra
+	
